1f3d03dcfa2a82e715c0de12536bdfa2924a3b68
SuriyaSR <suriyasun19@gmail.com>
2025-01-17 23:15:11 +0530

    Initial Code Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      Sample Project to generate strong password dynamically
+    </>
+  )
+}
+
+export default App
